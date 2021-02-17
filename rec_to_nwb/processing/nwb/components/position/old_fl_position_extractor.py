dc3ad50d84955b2320421a462f055b9533a6036c
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-02-16 20:02:52 -0800

    drop old_pos_timestamps_manager

--- rec_to_nwb/processing/nwb/components/position/old_fl_position_extractor.py
+++ rec_to_nwb/processing/nwb/components/position/old_fl_position_extractor.py
@@ -1,7 +1,8 @@
+from rec_to_nwb.processing.exceptions.missing_data_exception import MissingDataException
 from rec_to_nwb.processing.nwb.components.iterator.multi_thread_data_iterator import MultiThreadDataIterator
 from rec_to_nwb.processing.nwb.components.iterator.multi_thread_timestamp_iterator import MultiThreadTimestampIterator
-from rec_to_nwb.processing.nwb.components.position.old_pos_timestamp_manager import OldPosTimestampManager
 from rec_to_nwb.processing.nwb.components.position.pos_data_manager import PosDataManager
+from rec_to_nwb.processing.nwb.components.position.pos_timestamp_manager import PosTimestampManager
 from rec_to_nwb.processing.tools.beartype.beartype import beartype
 
 
@@ -10,17 +11,24 @@ class OldFlPositionExtractor:
     @beartype
     def __init__(self, datasets: list):
         self.datasets = datasets
-        self.all_pos = self.__extract_data()
+        self.all_pos, self.continuous_time = self.__extract_data()
 
     def __extract_data(self):
         all_pos = []
+        continuous_time = []
         for dataset in self.datasets:
             data_from_current_dataset = [
                 dataset.get_data_path_from_dataset('pos') + pos_file for pos_file in
                 dataset.get_all_data_from_dataset('pos') if
                 (pos_file.endswith('.pos_online.dat'))]
+            if dataset.get_continuous_time() is None:
+                raise MissingDataException(
+                    'Incomplete data in dataset '
+                    + str(dataset.name)
+                    + 'missing continuous time file')
             all_pos.append(data_from_current_dataset)
-        return all_pos
+            continuous_time.append(dataset.get_continuous_time())
+        return all_pos, continuous_time
 
     def get_positions(self):
         pos_datas = [
@@ -43,13 +51,15 @@ class OldFlPositionExtractor:
         ]
 
     def get_timestamps(self):
-        old_pos_timestamp_managers = [
-            OldPosTimestampManager(
+        pos_timestamp_managers = [
+            PosTimestampManager(
                 directories=[single_pos],
+                continuous_time_directories=[continuous_time],
+                convert_timestamps = False
             )
-            for single_pos in self.all_pos
+            for single_pos, continuous_time in zip(self.all_pos, self.continuous_time)
         ]
         return [
-            MultiThreadTimestampIterator(old_pos_timestamp_manager)
-            for old_pos_timestamp_manager in old_pos_timestamp_managers
+            MultiThreadTimestampIterator(pos_timestamp_manager)
+            for pos_timestamp_manager in pos_timestamp_managers
         ]
