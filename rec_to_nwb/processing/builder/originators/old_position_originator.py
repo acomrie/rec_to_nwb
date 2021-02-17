1ef7fe2082752713fc79ace3b180681b7b37ddb5
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-02-16 20:23:23 -0800

    drop old_fl_position_manager

--- rec_to_nwb/processing/builder/originators/old_position_originator.py
+++ rec_to_nwb/processing/builder/originators/old_position_originator.py
@@ -3,7 +3,7 @@ import os
 
 from pynwb import NWBFile
 
-from rec_to_nwb.processing.nwb.components.position.old_fl_position_manager import OldFlPositionManager
+from rec_to_nwb.processing.nwb.components.position.fl_position_manager import FlPositionManager
 from rec_to_nwb.processing.nwb.components.position.position_creator import PositionCreator
 from rec_to_nwb.processing.nwb.components.processing_module.processing_module_creator import ProcessingModuleCreator
 from rec_to_nwb.processing.tools.beartype.beartype import beartype
@@ -16,15 +16,18 @@ logger = logging.getLogger(__name__)
 class OldPositionOriginator:
 
     @beartype
-    def __init__(self, datasets: list, metadata: dict, dataset_names: list, process_timestamps: bool):
+    def __init__(self, datasets: list, metadata: dict, dataset_names: list,
+                    process_timestamps: bool,
+                    convert_timestamps: bool = False):
         self.pm_creator = ProcessingModuleCreator('behavior', 'Contains all behavior-related data')
-        self.old_fl_position_manager = OldFlPositionManager(datasets, metadata, dataset_names, process_timestamps)
+        self.fl_position_manager = FlPositionManager(datasets, metadata, dataset_names, process_timestamps,
+                                                    convert_timestamps=convert_timestamps)
         self.position_creator = PositionCreator()
 
     @beartype
     def make(self, nwb_content: NWBFile):
         logger.info('Position: Building')
-        fl_positions = self.old_fl_position_manager.get_fl_positions()
+        fl_positions = self.fl_position_manager.get_fl_positions()
         logger.info('Position: Creating')
         position = self.position_creator.create_all(fl_positions)
         logger.info('Position: Injecting into ProcessingModule')
