d5c7f12fb873dc0173cf173062eb751f581c6058
Ji Hyun Bak <jihyun.bak@gmail.com>
2021-02-16 23:40:38 -0800

    drop old_fl_video_files_manager

--- rec_to_nwb/processing/builder/originators/old_video_files_originator.py
+++ rec_to_nwb/processing/builder/originators/old_video_files_originator.py
@@ -1,5 +1,4 @@
 from rec_to_nwb.processing.nwb.components.video_files.fl_video_files_manager import FlVideoFilesManager
-from rec_to_nwb.processing.nwb.components.video_files.old_fl_video_files_manager import OldFlVideoFilesManager
 from rec_to_nwb.processing.nwb.components.video_files.video_files_creator import VideoFilesCreator
 from rec_to_nwb.processing.nwb.components.video_files.video_files_injector import VideoFilesInjector
 
@@ -8,10 +7,11 @@ class OldVideoFilesOriginator:
 
     def __init__(self, raw_data_path, video_path, video_files_metadata):
         self.video_directory = video_path
-        self.old_fl_video_files_manager = OldFlVideoFilesManager(raw_data_path, video_path, video_files_metadata)
+        self.fl_video_files_manager = FlVideoFilesManager(raw_data_path, video_path, video_files_metadata,
+                                                    return_timestamps=False)
 
     def make(self, nwb_content):
-        fl_video_files = self.old_fl_video_files_manager.get_video_files()
+        fl_video_files = self.fl_video_files_manager.get_video_files()
         image_series_list = [
             VideoFilesCreator.create(fl_video_file, self.video_directory, nwb_content)
             for fl_video_file in fl_video_files
